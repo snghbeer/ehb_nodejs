b1878ef99e5ad7acc73f0cd4214ea4b383b23df5
snghbeer <nivonsay.jean@hotmail.com>
2022-11-16 19:46:19 +0100

    adding crud for category model

--- controller/productController.js
+++ controller/productController.js
@@ -3,35 +3,45 @@ const dbURI = process.env.dbURI;
 
 const {Category, Product}  =  require('../models/product');
 
+//CATEGORY
 
-async function addNewCategory(data){
+async function addNewCategory(catName, dontClose){
+    var aCategory;
     await mongoose.connect(dbURI, {ssl:true})
     .then(async() =>{
         console.log("Adding new category...")
-        const aCategory = new Category({
-            name: data.name
+        aCategory = new Category({
+            name: catName
         })
-        await aCategory.save().then(() => mongoose.connection.close());
+        if(!dontClose){
+            await aCategory.save().then(() => mongoose.connection.close());
+        }
     })
+    return aCategory;
 }
 
-async function addNewProduct(data){
+async function updateCategory(newData){
+    var aCategory;
     await mongoose.connect(dbURI, {ssl:true})
     .then(async() =>{
-        console.log("Adding new product...")
-        const aCat = await Category.findOne({name: data.category}).exec()
-        const aProduct = new Product({
-            name: data.name,
-            price: data.price,
-            description: data.description,
-            category: aCat.id
-        })
-        await aProduct.save().then(async () => {
-            aCat.products.push(aProduct);
-            await aCat.save();
-            mongoose.connection.close()
-        });
+        console.log("Updating product with ID: "+ id)
+        aCategory = await Category.findOneAndUpdate({name: category}, newData, {new: true}).exec();
+        mongoose.connection.close()
     })
+    return aCategory;
+}
+
+async function deleteCategory(id){
+    var deleted = false;
+    await mongoose.connect(dbURI, {ssl:true})
+    .then(async() =>{
+        console.log("Deleting product with ID: "+ id)
+        await Category.deleteOne({_id: id});
+        await Product.deleteMany({category: id}); //cascade delete
+        deleted = true
+        mongoose.connection.close()
+    })
+    return deleted;
 }
 
 async function getCategories(){
@@ -45,6 +55,8 @@ async function getCategories(){
     return cats;
 }
 
+//PRODUCT
+
 async function getProducts(category){
     var prods;
     await mongoose.connect(dbURI, {ssl:true})
@@ -57,10 +69,91 @@ async function getProducts(category){
     return prods.products;
 }
 
+async function addNewProduct(data){
+    await mongoose.connect(dbURI, {ssl:true})
+    .then(async() =>{
+        console.log("Adding new product...")
+        const aCat = await Category.findOne({name: data.category}).exec()
+        if(aCat){
+            const aProduct = new Product({
+                name: data.name,
+                price: data.price,
+                description: data.description,
+                category: aCat.id
+            })
+            await aProduct.save().then(async () => {
+                aCat.products.push(aProduct);
+                await aCat.save();
+                mongoose.connection.close()
+            });
+        }
+        else{
+            console.log("Category not found for this product...")
+            await addNewCategory(data.category, true).then(async(newCategory) => {
+                console.log(newCategory)
+                const aProduct = new Product({
+                    name: data.name,
+                    price: data.price,
+                    description: data.description,
+                    category: newCategory.id
+                })
+                await aProduct.save().then(async () => {
+                    newCategory.products.push(aProduct);
+                    await newCategory.save();
+                    mongoose.connection.close()
+                });
+            });
+
+        }
+    })
+}
+
+async function getProduct(id){
+    var prod;
+    await mongoose.connect(dbURI, {ssl:true})
+    .then(async() =>{
+        console.log("Fetching product with ID: "+ id)
+        prod = await Product.findById(id).exec();
+        //console.log(prods.products)
+        mongoose.connection.close()
+    })
+    return prod;
+}
+
+async function updateProduct(id, newData){
+    var prod;
+    await mongoose.connect(dbURI, {ssl:true})
+    .then(async() =>{
+        console.log("Fetching product with ID: "+ id)
+        prod = await Product.findByIdAndUpdate(id, newData, {new: true}).exec();
+        //console.log(prods.products)
+        mongoose.connection.close()
+    })
+    return prod;
+}
+
+async function deleteProduct(id){
+    var deleted = false;
+    await mongoose.connect(dbURI, {ssl:true})
+    .then(async() =>{
+        console.log("Deleting product with ID: "+ id)
+        await Product.deleteOne({_id: id});
+        deleted = true
+        mongoose.connection.close()
+    })
+    return deleted;
+}
+
 module.exports = { 
     addNewCategory: addNewCategory,
+    updateCategory: updateCategory,
+    deleteCategory: deleteCategory,
+    getCategories: getCategories,
+
     addNewProduct: addNewProduct,
     getProducts: getProducts,
-    getCategories: getCategories,
+    getProduct: getProduct,
+    updateProduct: updateProduct,
+    deleteProduct: deleteProduct,
  }
 
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 require('express-router-group');
-var bodyParser = require('body-parser')
+const {addNewCategory, addNewProduct, getProducts, getCategories, getProduct,
+     updateProduct, deleteProduct, deleteCategory, updateCategory} = require('../controller/productController');
+var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
-const {addNewCategory, addNewProduct, getProducts, getCategories}  =  require('../controller/productController');
 
 const app = express()
 const homeRouter = express.Router()
@@ -15,44 +16,92 @@ homeRouter.group("/v1", (homeRouter) => {
         res.send("Test api")
      //   console.log(req.cookies)
     });
-    homeRouter.post("/category/new", jsonParser, function(req, res){
-        console.log(req.body)
-        const data = req.body;
-        addNewCategory(data).then(() =>{
-            res.status(200).send("Category added succesfully!")
+
+    homeRouter.group("/category", (homeRouter) => {
+        homeRouter.post("/new", jsonParser, function(req, res){
+            console.log(req.body)
+            const data = req.body.name;
+            addNewCategory(data).then(() =>{
+                res.status(200).send("Category added succesfully!")
+            });
+        });
+
+        homeRouter.get("/:cat/products", async function(req, res){
+            const data = req.params.cat;
+            var products;
+            try{
+                products = await getProducts(data)
+                res.status(200).send(products)
+            }
+            catch(err){
+                console.log(err)
+            }
+        });
+
+        homeRouter.get("/all", jsonParser, async function(req, res){
+            const data = req.params.cat;
+            var cats;
+            try{
+                cats = await getCategories()
+                res.status(200).send(cats)
+            }
+            catch(err){
+                console.log(err)
+            }
+        });
+
+        homeRouter.delete("/:id", async function(req, res){
+            const id = req.params.id;
+            const deleted = await deleteCategory(id)
+            if(deleted) res.status(200).send("Category deleted succesfully!")
+            else res.status(404).send("Category does not exist");
+
+        });
+
+        homeRouter.put("/:id", async function(req, res){
+            const id = req.params.id;
+            const deleted = await updateCategory(id)
+            if(deleted) res.status(200).send("Category updated succesfully!")
+            else res.status(404).send("Category does not exist");
+
         });
     });
 
-    homeRouter.post("/product/new", jsonParser, function(req, res){
-        console.log(req.body)
-        const data = req.body;
-        addNewProduct(data).then(() =>{
-            res.status(200).send("Product added succesfully!")
+    homeRouter.group("/product", (homeRouter) => {
+        homeRouter.post("/new", jsonParser, function(req, res){
+            console.log(req.body)
+            const data = req.body;
+            addNewProduct(data).then(() =>{
+                res.status(200).send("Product added succesfully!")
+            });
         });
-    });
 
-    homeRouter.get("/:cat/products", jsonParser, async function(req, res){
-        const data = req.params.cat;
-        var products;
-        try{
-            products = await getProducts(data)
-            res.status(200).send(products)
-        }
-        catch(err){
-            console.log(err)
-        }
-    });
+        homeRouter.get("/:id", jsonParser, async function(req, res){
+            const id = req.params.id;
+            const product = await getProduct(id);
+            console.log(product);
+            if(product) res.status(200).send(product)
+            else res.status(404).send("Not found");
+        });
 
-    homeRouter.get("/categories", jsonParser, async function(req, res){
-        const data = req.params.cat;
-        var cats;
-        try{
-            cats = await getCategories()
-            res.status(200).send(cats)
-        }
-        catch(err){
-            console.log(err)
-        }
+        homeRouter.put("/:id", jsonParser, async function(req, res){
+            const id = req.params.id;
+            const newData = req.body;
+            console.log(newData)
+            const updatedProd = await updateProduct(id, newData);
+            if(updatedProd) res.status(200).send("Product updated succesfully with fields: " + updatedProd)
+            else res.status(404).send("Couldn't update product");
+
+        });
+
+        homeRouter.delete("/:id", async function(req, res){
+            const id = req.params.id;
+            console.log("Deleting")
+            const deleted = await deleteProduct(id)
+            if(deleted) res.status(200).send("Product deleted succesfully!")
+            else res.status(404).send("Product does not exist");
+
+        });
     });
 });
 
